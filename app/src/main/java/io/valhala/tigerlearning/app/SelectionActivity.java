1b42e9bf48e2b4c8771d9027ecdf16bea1c0e232
mlizbeth <mirgantrophy@gmail.com>
2018-07-30 13:59:25 -0500

    vertical checkboxes for selections

--- app/src/main/java/io/valhala/tigerlearning/app/SelectionActivity.java
+++ app/src/main/java/io/valhala/tigerlearning/app/SelectionActivity.java
@@ -8,6 +8,6 @@ public class SelectionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selection);
+        setContentView(R.layout.activity_checkbox_t);
     }
 }
